980ac1c753927b0f9e879b194fcd66f757701553
tingfei <1228959039@qq.com>
2023-06-27 00:10:56 +0800

    add /api/data

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,7 +1,21 @@
 import express from "express";
+import { Pool } from "pg";
+import { PoolData } from "../../database";
 
 const app = express();
 const port = 3000;
+
+const pool = new Pool(PoolData);
+
+app.get("/api/data", async (req, res) => {
+  try {
+    const { rows } = await pool.query("SELECT * FROM dapp_sites");
+    res.json(rows);
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).json({ error: "An error occurred" });
+  }
+});
 
 app.get("/", (req, res) => {
   res.send("Hello, World! Welcome to the Whitelist API!");
